d428d27b881e5344472b49f384f224ff748ada07
Stefan Gavriliuc <stefan.gavriliuc@ucalgary.ca>
2020-04-13 16:00:41 -0600

    documented classes in allusers package

--- Version 2.0/src/allusers/Patient.java
+++ Version 2.0/src/allusers/Patient.java
@@ -1,3 +1,12 @@
+/**
+ * Basic functionalities for users of account type patient
+ * Currently does nothing.
+ * 
+ * @author Dheeraj
+ * @version 3.0
+ * @since 2.0
+ */
+
 package allusers;
 
 public class Patient {
