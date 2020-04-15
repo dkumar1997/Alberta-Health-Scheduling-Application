2590352071c4efc422b41e5bb1a44cf1f98d0a5c
nickwoz <60114682+nickwoz@users.noreply.github.com>
2020-04-14 19:32:07 -0600

    version 5.0

--- Version 2.0/src/hospital_gui/Calendar.java
+++ Version 2.0/src/hospital_gui/Calendar.java
@@ -11,6 +11,9 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.swt.widgets.Label;
+
+import java.util.Arrays;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.wb.swt.SWTResourceManager;
 
@@ -97,156 +100,42 @@ public class Calendar {
 		lblFriday.setText("Friday");
 		
 		
-		Button btnNewButton = new Button(shell, SWT.NONE);
-		btnNewButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
+		int weekend[]= {1,7,8,14,15,21,22,28,29};
+		
+		int s = 0;	
+		int x = 0;
+		int y = 120;
+		for (int i = 1; i <= 31; i++) {			
+			int day = i;
+			
+			if(0<=Arrays.binarySearch(weekend, day)){//if weekend don't add button		
 				
-				Booking march2 = new Booking(user_id,role);
-				march2.setDay(2);
-				march2.open();
+			}else {
+			
+				Button btnNewButton_3 = new Button(shell, SWT.NONE);
+				btnNewButton_3.addSelectionListener(new SelectionAdapter() {
+					@Override
+					public void widgetSelected(SelectionEvent e) {
+						Booking march = new Booking(user_id,role);
+						march.setDay(day);
+						march.open();
+	
+						
+					}
+				});
+			
+				btnNewButton_3.setText(Integer.toString(day));
+				btnNewButton_3.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
+				x = 28 + (152 * s);
+				btnNewButton_3.setBounds(x, y, 76, 51);			
+				s++;	
 			}
-		});
-		btnNewButton.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton.setBounds(28, 108, 76, 51);
-		btnNewButton.setText("2");
-		
-		Button btnNewButton_1 = new Button(shell, SWT.NONE);
-		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				Booking march2 = new Booking(user_id,role);
-				march2.setDay(3);
-				march2.open();
+			if(day==7 || day==14 || day== 21 || day ==28) {//new week/row
+				y=y+120;
+				s=0;
 			}
-		});
-		btnNewButton_1.setText("3");
-		btnNewButton_1.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_1.setBounds(180, 108, 76, 51);
-		
-		Button btnNewButton_2 = new Button(shell, SWT.NONE);
-		btnNewButton_2.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				Booking march2 = new Booking(user_id,role);
-				march2.setDay(4);
-				march2.open();
-			}
-		});
-		btnNewButton_2.setText("4");
-		btnNewButton_2.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_2.setBounds(346, 108, 76, 51);
-		
-		Button btnNewButton_3 = new Button(shell, SWT.NONE);
-		btnNewButton_3.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				Booking march2 = new Booking(user_id,role);
-				march2.setDay(5);
-				march2.open();
-			}
-		});
-		btnNewButton_3.setText("5");
-		btnNewButton_3.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_3.setBounds(511, 108, 76, 51);
-		
-		Button btnNewButton_4 = new Button(shell, SWT.NONE);
-		btnNewButton_4.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				Booking march2 = new Booking(user_id,role);
-				march2.setDay(6);
-				march2.open();
-			}
-		});
-		btnNewButton_4.setText("6");
-		btnNewButton_4.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_4.setBounds(669, 108, 76, 51);
-		
-		Button btnNewButton_5 = new Button(shell, SWT.NONE);
-		btnNewButton_5.setText("9");
-		btnNewButton_5.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_5.setBounds(30, 220, 76, 51);
-		
-		Button btnNewButton_6 = new Button(shell, SWT.NONE);
-		btnNewButton_6.setText("10");
-		btnNewButton_6.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_6.setBounds(180, 220, 76, 51);
-		
-		Button btnNewButton_7 = new Button(shell, SWT.NONE);
-		btnNewButton_7.setText("11");
-		btnNewButton_7.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_7.setBounds(346, 220, 76, 51);
-		
-		Button btnNewButton_8 = new Button(shell, SWT.NONE);
-		btnNewButton_8.setText("12");
-		btnNewButton_8.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_8.setBounds(511, 220, 76, 51);
-		
-		Button btnNewButton_9 = new Button(shell, SWT.NONE);
-		btnNewButton_9.setText("13");
-		btnNewButton_9.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_9.setBounds(669, 220, 76, 51);
-		
-		Button btnNewButton_10 = new Button(shell, SWT.NONE);
-		btnNewButton_10.setText("16");
-		btnNewButton_10.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_10.setBounds(30, 343, 76, 51);
-		
-		Button btnNewButton_11 = new Button(shell, SWT.NONE);
-		btnNewButton_11.setText("17");
-		btnNewButton_11.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_11.setBounds(180, 343, 76, 51);
-		
-		Button btnNewButton_12 = new Button(shell, SWT.NONE);
-		btnNewButton_12.setText("18");
-		btnNewButton_12.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_12.setBounds(346, 343, 76, 51);
-		
-		Button btnNewButton_13 = new Button(shell, SWT.NONE);
-		btnNewButton_13.setText("19");
-		btnNewButton_13.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_13.setBounds(511, 343, 76, 51);
-		
-		Button btnNewButton_14 = new Button(shell, SWT.NONE);
-		btnNewButton_14.setText("20");
-		btnNewButton_14.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_14.setBounds(669, 343, 76, 51);
-		
-		Button btnNewButton_15 = new Button(shell, SWT.NONE);
-		btnNewButton_15.setText("23");
-		btnNewButton_15.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_15.setBounds(30, 475, 76, 51);
-		
-		Button btnNewButton_16 = new Button(shell, SWT.NONE);
-		btnNewButton_16.setText("24");
-		btnNewButton_16.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_16.setBounds(180, 475, 76, 51);
-		
-		Button btnNewButton_17 = new Button(shell, SWT.NONE);
-		btnNewButton_17.setText("25");
-		btnNewButton_17.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_17.setBounds(346, 475, 76, 51);
-		
-		Button btnNewButton_18 = new Button(shell, SWT.NONE);
-		btnNewButton_18.setText("26");
-		btnNewButton_18.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_18.setBounds(511, 475, 76, 51);
-		
-		Button btnNewButton_19 = new Button(shell, SWT.NONE);
-		btnNewButton_19.setText("27");
-		btnNewButton_19.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_19.setBounds(669, 475, 76, 51);
-		
-		Button btnNewButton_20 = new Button(shell, SWT.NONE);
-		btnNewButton_20.setText("30");
-		btnNewButton_20.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_20.setBounds(30, 594, 76, 51);
-		
-		Button btnNewButton_21 = new Button(shell, SWT.NONE);
-		btnNewButton_21.setText("31");
-		btnNewButton_21.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
-		btnNewButton_21.setBounds(180, 594, 76, 51);
+			
+		}
 		
 		Button btnBackToMain = new Button(shell, SWT.NONE);
 		btnBackToMain.addSelectionListener(new SelectionAdapter() {
